49ae3995c7652ff7adb46a4edfd489b86e0d9b8c
Dutchman <meelisfidelis@gmail.com>
2022-05-30 01:00:34 +0100

    Animate text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,13 +13,22 @@ const Home = () => {
     return(
         <div className = 'container home-page'>
             <div className = 'text-zone'>
-                <h1>Hi, <br/> I'm
+                <h1>
+                <span className = {letterClass}>H</span>
+                <span className = {'${letterClass} _12'}>i</span>
+                    <br/>
+                <span className = {'${letterClass} _13'}>I</span>
+                <span className = {'${letterClass} _14'}>'m</span> 
                 <img src = {LogoTitle} alt = 'developer' />
                 <AnimatedLetters letterClass = {letterClass}
                 strArray ={nameArray}
                 idx ={15}/>
                 <br/>
-                software developer
+
+                <AnimatedLetters letterClass = {letterClass}
+                strArray = {jobArray}
+                idx ={22}/>
+
                 </h1>
                 <h2> Blockchain Developer / Fullstack Developer </h2>
                 <Link to ='/contact' className = 'flat-button'> CONTACT ME </Link>  
