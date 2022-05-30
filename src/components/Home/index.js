016e162bb96a3664330dc6921262ef171a6485d4
Dutchman <meelisfidelis@gmail.com>
2022-05-30 01:40:54 +0100

    animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,18 +8,19 @@ import './index.scss'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray =['o', 'b', 'o', 'd', 'a', 'n']
-    const jobArray =['s','o', 'f', 't', 'w', 'a', 'r','e','', 'd', 'e', 'v', 'l', 'o', 'p', 'e', 'r']
+    const jobArray =['s','o', 'f', 't', 'w', 'a', 'r','e',' ', ' d', 'e', 'v', 'l', 'o', 'p', 'e', 'r']
 
     return(
         <div className = 'container home-page'>
             <div className = 'text-zone'>
                 <h1>
                 <span className = {letterClass}>H</span>
-                <span className = {'${letterClass} _12'}>i</span>
-                    <br/>
-                <span className = {'${letterClass} _13'}>I</span>
-                <span className = {'${letterClass} _14'}>'m</span> 
+                <span className = {`${letterClass} _12`}>i</span>
+                <br/>
+                <span className = {`${letterClass} _13`}>I</span>
+                <span className = {`${letterClass} _14`}>'m</span> 
                 <img src = {LogoTitle} alt = 'developer' />
+
                 <AnimatedLetters letterClass = {letterClass}
                 strArray ={nameArray}
                 idx ={15}/>
